ff1ef1217eb722b7ad53ff4dfc963cbddf91b823
zhangshuang <zhangshuang@qfpay.com>
2015-08-27 12:08:35 +0800

    xxxx

--- dict_to_xml.py
+++ dict_to_xml.py
@@ -1,62 +1,77 @@
 #coding: utf-8
 import types
 
+#输入的dict必须都是unicode编码
 
 class XmlDict(object):
-    def __init__(self, encode='utf-8'):
-        self.encode = encode
+    def __init__(self, root=True, custom_root=None, encode='utf-8'):
+        self.encode = encode 
+        self.root = root
+        self.custom_root = u'root' if custom_root is None else custom_root
 
-    def __list__value__(self, v):
-        xml_value_str_list = [] 
+    def unparse_list(self, v):
+        xml_list_value = [] 
         for e in v:
-            xml_value_str_list.append('<item>') 
-            xml_value_str_list.append(self.__parse_value__(e)) 
-            xml_value_str_list.append('</item>')
-        return ''.join(xml_value_str_list)
+            xml_list_value.append(u'<item>') 
+            xml_list_value.append(self.unparse_value(e)) 
+            xml_list_value.append(u'</item>')
+        return u''.join(xml_list_value)
             
-    def __parse_value__(self, v):
+    def unparse_value(self, v):
         if isinstance(v, types.UnicodeType): 
-            return '<![CDATA[%s]]>' % v.encode('utf-8')
+            return u'<![CDATA[%s]]>' % v
         elif isinstance(v, dict):
-            return self.__convert_dict_to_xml__(v)
+            return self.unparse_dict(v)
         elif isinstance(v, (list, tuple)):
-            return self.__list__value__(v) 
+            return self.unparse_list(v) 
         elif v is None:
-            return '<![CDATA[]]>'
+            return u'<![CDATA[]]>'
         else:
-            return '<![CDATA[%s]]>' % str(v)
+            return u'<![CDATA[%s]]>' % v
 
-    def __parse_key__(self, key):
-        if isinstance(key, types.UnicodeType):
-            return key.encode('utf-8')
-        else:
-            return str(key)
-        
-    def __convert_dict_to_xml__(self, params):
+    def unparse_dict(self, d):
         xml_str_list = [] 
-        for k, v in params.iteritems():
-            k = self.__parse_key__(k)
-            xml_str_list.append('<%s>' % k)
-            xml_str_list.append(self.__parse_value__(v))
-            xml_str_list.append('</%s>' % k)
-
-        return ''.join(xml_str_list)
+        for k, v in d.iteritems():
+            xml_str_list.append(u'<%s>' % k)
+            xml_str_list.append(self.unparse_value(v))
+            xml_str_list.append(u'</%s>' % k)
+        return u''.join(xml_str_list)
 
     #将一个字典转化成xml
-    def convert_dict_to_xml(self, params):
+    def convert_dict_to_xml(self, d):
         xml_str_list = []
-        xml_str_list.append(self.__convert_dict_to_xml__(params)) 
-        if self.encode is None:
-            return ''.join(xml_str_list).decode('utf-8')
+        xml_str_list.append(u'<?xml version="1.0" encoding="%s" ?>' % self.encode)
+        if self.root:
+            xml_str_list.append(u'<%s>' % self.custom_root)
+        xml_str_list.append(self.unparse_dict(d)) 
+        if self.root:
+            xml_str_list.append(u'</%s>' % self.custom_root)
+        s = u''.join(xml_str_list)
+        if self.encode is not None:
+            return s.encode(self.encode) 
         else:
-            return ''.join(xml_str_list).decode('utf-8').encode(self.encode)
+            return s.encode('utf-8')
 
-if __name__ == '__main__':
+def test1():
     params = {'test': 'a', 'test1': 1, "test234": [1,2,3,4,5], 'None': None} 
-    params['dict'] = {'xxxxxxxxxxx': 'bbbbbbbbbbbbb', u'马达': '测试'}
-    xml_str = XmlDict().convert_dict_to_xml({'xml': params})
+    params['dict'] = {'xxxxxxxxxxx': 'bbbbbbbbbbbbb', u'马达': u'测试'}
+    xml_str = XmlDict(root=True, custom_root='xml').convert_dict_to_xml(params)
     print xml_str
     import xmltodict
     rsp_dict = xmltodict.parse(xml_str)
     for k, v in rsp_dict['xml'].iteritems():
         print k, ':', v
+def test2():
+    params = {'test': 'a', 'test1': 1, "test234": [1,2,3,4,5], 'None': None} 
+    params['dict'] = {'xxxxxxxxxxx': 'bbbbbbbbbbbbb', 'testxxxxx': u'测试'}
+    xml_str = XmlDict(root=True, custom_root='xml', encode='gb2312').convert_dict_to_xml(params)
+    print xml_str
+    import xmltodict
+    #import xmlparser
+    #rsp_dict = xmlparser.XmlDict(xml_str).to_dict()
+    rsp_dict = xmltodict.parse(xml_str.decode('gb2312'))
+    for k, v in rsp_dict['xml'].iteritems():
+        print k, ':', v
+if __name__ == '__main__':
+    test1()
+    test2()
